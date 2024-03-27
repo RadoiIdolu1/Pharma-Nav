2785d89fa65741387af9c72f7c1d9179078697e0
Radu Mihai-Alexandru <124779623+RaduM10@users.noreply.github.com>
2024-03-27 19:07:26 +0200

    Dropdown menu
    Added dropwdown menu, moved style inside css  and a script for the menu in the typescript file

--- pharma-nav/src/app/home/home.component.ts
+++ pharma-nav/src/app/home/home.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  isMenuOpen: boolean = false;
 
+  constructor() { }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
